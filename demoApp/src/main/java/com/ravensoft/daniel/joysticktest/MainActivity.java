3e225f0a8310d0dd6469175a679f33c9ef02cf13
harry1453 <harryjamesphillips@gmail.com>
2018-05-18 21:23:16 +0100

    Redesign demo app

--- demoApp/src/main/java/com/ravensoft/daniel/joysticktest/MainActivity.java
+++ demoApp/src/main/java/com/ravensoft/daniel/joysticktest/MainActivity.java
@@ -2,18 +2,35 @@ package com.ravensoft.daniel.joysticktest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.widget.TextView;
 
 import com.harrysoft.joystickview.JoystickView;
 
+import java.text.DecimalFormat;
+
 public class MainActivity extends AppCompatActivity implements JoystickView.JoystickListener{
+
+    private DecimalFormat decimalFormat;
+    private TextView leftX, leftY, rightX, rightY;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        JoystickView left = findViewById(R.id.joystickLeft);
-        JoystickView right = findViewById(R.id.joystickRight);
+
+        leftX = findViewById(R.id.left_x);
+        leftY = findViewById(R.id.left_y);
+        rightX = findViewById(R.id.right_x);
+        rightY = findViewById(R.id.right_y);
+        JoystickView left = findViewById(R.id.left_joystick);
+        JoystickView right = findViewById(R.id.right_joystick);
+        
+        decimalFormat = new DecimalFormat("#.###");
+
+        // Initialize display with zero values
+        onJoystickMoved(0f, 0f, R.id.left_joystick);
+        onJoystickMoved(0f, 0f, R.id.right_joystick);
+
         left.setJoystickListener(this);
         right.setJoystickListener(this);
     }
@@ -21,11 +38,13 @@ public class MainActivity extends AppCompatActivity implements JoystickView.Joys
     @Override
     public void onJoystickMoved(float xPercent, float yPercent, int id) {
         switch (id) {
-            case R.id.joystickRight:
-                Log.d("Right Joystick", "X percent: " + xPercent + " Y percent: " + yPercent);
+            case R.id.left_joystick:
+                leftX.setText(getString(R.string.x_pos_format, decimalFormat.format(xPercent)));
+                leftY.setText(getString(R.string.y_pos_format, decimalFormat.format(yPercent)));
                 break;
-            case R.id.joystickLeft:
-                Log.d("Left Joystick", "X percent: " + xPercent + " Y percent: " + yPercent);
+            case R.id.right_joystick:
+                rightX.setText(getString(R.string.x_pos_format, decimalFormat.format(xPercent)));
+                rightY.setText(getString(R.string.y_pos_format, decimalFormat.format(yPercent)));
                 break;
         }
     }
